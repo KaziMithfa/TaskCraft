0648d780b3c6ead8c8eea8c395ac57ee18634a73
KaziMithfa <kazimithfa99@gmail.com>
2025-08-08 01:20:10 +0200

    deployment in vercel completed

--- taskcraft-server/index.js
+++ taskcraft-server/index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://taskcraft-b8bea.web.app",
+    "https://taskcraft-rho.vercel.app",
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
